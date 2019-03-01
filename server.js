53098d7eae92126e7d612b24fb92fc8aba94a5df
erikmjohnson <erikarisma98@gmail.com>
2019-02-28 16:06:43 -0800

    Working on time stamp

--- server.js
+++ server.js
@@ -6,20 +6,46 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(requestTime);
+
 app.get('/a', (req,res) => {
   res.status(200).send('Route A');
+  res.send(`${req.requestTime}`);
 });
 
 app.get('/b', (req,res) => {
   res.status(200).send('Route B');
+  res.send(`${req.requestTime}`);
 });
 
 app.get('/c', (req,res) => {
   res.status(200).send('Route C');
+  res.send(`${req.requestTime}`);
 });
 
 app.get('/d', (req,res) => {
   res.status(200).send('Route D');
+  res.send(`${req.requestTime}`);
+});
+
+// app.use((req, res, error, next) => {
+//   console.log('ERROR!!!');
+//   res.status(500);
+//   res.send('Error!');
+// });
+
+app.get('/*', (req, res) => {
+  res.status(404).send('Route Not Found');
+
+});
+
+app.get('/', function(req, res) {
+  throw new Error('You broke me!');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
